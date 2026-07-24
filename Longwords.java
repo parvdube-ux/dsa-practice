94e005ec7698baf38a0f0a04481f6e2869b367a1
parvdube-ux <parvdube@gmail.com>
2026-07-24 12:49:36 +0530

    Rename Main class to Longwords and refactor

--- Longwords.java
+++ Longwords.java
@@ -1,7 +1,7 @@
 import java.lang.String;
 import java.util.Scanner;
 
-public class Main {
+public class Longwords {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
