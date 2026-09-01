3f69a30b4470ea0be053e25e0e1e4957e6796816
parvdube-ux <parvdube@gmail.com>
2026-09-01 23:02:13 +0530

    Add files via upload

---
+++ LargestinArray.java
@@ -0,0 +1,18 @@
+public class LargestinArray {
+    public static void main(String[] args){
+        int arr[] = {2,5,1,3,0};
+
+        int largest = arr[0];
+
+        largest = arr[0];
+
+        for(int i=0; i<arr.length;i++){
+            if(arr[i] > largest){
+                largest = arr[i];
+            }
+        }
+
+        System.out.println(largest);
+
+    }
+}
