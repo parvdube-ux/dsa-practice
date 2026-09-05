891fa98d5227e811b91405804c731693afa96607
parvdube-ux <parvdube@gmail.com>
2026-09-05 23:00:35 +0530

    Add files via upload

---
+++ RotArr.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class RotArr {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+
+        static void reverse(int arr[]){
+            int left = 0;
+            int right = arr.length - 1;
+
+            while (left < right) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+
+                left++;
+                right--;
+            }
+
+            for (int x : arr) {
+                System.out.print(x + " ");
+            }
+        }
+
+
+    }
+}
